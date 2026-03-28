20bada590bdb3482e5dd4928b463c82d8f35d175
Levente Feher <levi@leventefeher.com>
2026-03-28 23:46:52 +0000

    Use trailingSlash: always to match Cloudflare Pages behaviour

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://www.bailacanarias.com',
   output: 'static',
-  trailingSlash: 'never',
+  trailingSlash: 'always',
   adapter: node({ mode: 'standalone' }),
   integrations: [react(), keystatic(), sitemap()],
   i18n: {
--- astro.config.prod.mjs
+++ astro.config.prod.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://www.bailacanarias.com',
   output: 'static',
-  trailingSlash: 'never',
+  trailingSlash: 'always',
   integrations: [react(), sitemap()],
   i18n: {
     defaultLocale: 'en',
